b225f194964e6f21ada4a794b57da7a1794d7632
rasna shakya <rasnashakya@gmail.com>
2017-02-06 15:44:52 +0545

    password valdation by length was added

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,7 +1,10 @@
 class User < ApplicationRecord
-	validates_presence_of :name, :email, :password
-	validates :terms_of_services, acceptance: true
-	validates :password, confirmation:{case_sensitive: true}
-	validates :password_confirmation, presence: true
+  validates_presence_of :name, :email, :password
+  validates :terms_of_services, acceptance: true
+  validates :password, confirmation: {case_sensitive: true},
+                       length: {in: 5..15}
+  validates :password_confirmation, presence: true
+  # validates :name, exclusion: { in: %w(_ @ - $),
+  #   message: "%{value} is not a valid, no special character" }
 
 end
